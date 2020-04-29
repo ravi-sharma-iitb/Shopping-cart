74d610d4bb2b6dc7deaa3f19b354c4f0d4d0cd4c
shubham <kumarsinghshubham32111@gmail.com>
2020-04-29 16:36:58 +0530

    bug fixed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://theclosecompany.com/",
+  baseURL: "https://appserver.theclosecompany.com/",
   withCredentials: true
 });
